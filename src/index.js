9928c1fd321c1f200ee920e7a394b63783a59d8c
zeng715734199 <715734199@qq.com>
2021-08-07 23:22:07 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import xxx from "./x.js";
 import png from "../assets/1.png";
-console.log(png);
+// console.log(png);
 
 const div = document.getElementById("app");
-div.innerHTML = `<img src="${png}">`;
+// div.innerHTML = `<img src="${png}">`;
 
 const button = document.createElement("button");
 button.innerText = "懒加载";
